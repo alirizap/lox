6292e226ecdbdf55fa767e090b018f02dc90451e
Alireza Pishdar <pishdaralirza@gmail.com>
2026-07-29 17:20:48 +0430

    feat: add generated AST node classes

---
+++ plox/plox/expr.py
@@ -0,0 +1,69 @@
+"""
+Expr AST node definations.
+GENERATED CODE - produced by tool/generate_ast.py. do not edit by hand.
+"""
+
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Any
+from plox.token import Token
+
+
+class Expr(ABC):
+    @abstractmethod
+    def accept(self, visitor: ExprVisitor) -> Any:
+        raise NotImplementedError
+
+
+class ExprVisitor(ABC):
+    @abstractmethod
+    def visit_binary_expr(self, expr: Binary) -> Any:
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_grouping_expr(self, expr: Grouping) -> Any:
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_literal_expr(self, expr: Literal) -> Any:
+        raise NotImplementedError
+
+    @abstractmethod
+    def visit_unary_expr(self, expr: Unary) -> Any:
+        raise NotImplementedError
+
+
+@dataclass(frozen=True)
+class Binary(Expr):
+    left: Expr
+    operator: Token
+    right: Expr
+
+    def accept(self, visitor: ExprVisitor) -> Any:
+        return visitor.visit_binary_expr(self)
+
+
+@dataclass(frozen=True)
+class Grouping(Expr):
+    expression: Expr
+
+    def accept(self, visitor: ExprVisitor) -> Any:
+        return visitor.visit_grouping_expr(self)
+
+
+@dataclass(frozen=True)
+class Literal(Expr):
+    value: Any
+
+    def accept(self, visitor: ExprVisitor) -> Any:
+        return visitor.visit_literal_expr(self)
+
+
+@dataclass(frozen=True)
+class Unary(Expr):
+    operator: Token
+    right: Expr
+
+    def accept(self, visitor: ExprVisitor) -> Any:
+        return visitor.visit_unary_expr(self)
