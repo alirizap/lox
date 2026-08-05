eb05cc6a174a59dcadef2a87f6c44319758c1ccb
Alireza Pishdar <pishdaralirza@gmail.com>
2026-08-05 15:27:22 +0430

    feat: add enviroment for holding declarations

---
+++ plox/plox/enviroment.py
@@ -0,0 +1,17 @@
+from typing import Any
+from plox.token import Token
+from plox.errors import LoxRuntimeError
+
+
+class Enviroment:
+    def __init__(self) -> None:
+        self.values = dict()
+
+    def define(self, name: str, value: Any) -> None:
+        self.values[name] = value
+
+    def get(self, name: Token) -> Any:
+        if name in self.values:
+            return self.values[name]
+
+        raise LoxRuntimeError(name, f"Undefined variable '{name.lexeme}'.")
